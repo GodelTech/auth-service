38f2c4b9e8a821f7ba07a71cb6cd834f4d082ddb
m.wasielewski <m.wasielewski@godeltech.com>
2022-11-22 23:35:10 +0100

    identity_resource_tables created

---
+++ src/data_access/postgresql/tables/identity_resource_tables.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class IdentityClaims(Base):
+    __tablename__ = "identity_claims"
+
+    id = Column(Integer, primary_key=True)
+    identity_resource_id = Column(Integer, ForeignKey("identity_resources.id"))
+    type = Column(Text)
+
+    def __str__(self) -> str:
+        return self.__tablename__
+
+
+class IdentityResourses(Base):
+    __tablename__ = "identity_resources"
+
+    id = Column(Integer, primary_key=True)
+    description = Column(Text)
+    display_name = Column(String, nullable=False)
+    emphasize = Column(Boolean) # this record needs to be emphasized or not?
+    enabled = Column(Boolean, nullable=False)
+    name = Column(String)
+    required = Column(Boolean)
+    show_in_discovery_document = Column(Boolean)
+
+    def __str__(self) -> str:
+        return self.__tablename__
