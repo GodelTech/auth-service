b3f6b493be032d8e3f928bc1983ff476049f3d1b
m.wasielewski <m.wasielewski@godeltech.com>
2022-11-24 15:46:37 +0100

    Identity Resourse table

--- src/data_access/postgresql/tables/identity_resource_tables.py
+++ src/data_access/postgresql/tables/identity_resource_tables.py
@@ -1,30 +1,31 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Text
+from sqlalchemy_utils import ChoiceType
 
-from .base import Base
+from .base import BaseModel
 
 
-class IdentityClaims(Base):
+class IdentityClaims(BaseModel):
     __tablename__ = "identity_claims"
 
     id = Column(Integer, primary_key=True)
     identity_resource_id = Column(Integer, ForeignKey("identity_resources.id"))
-    type = Column(Text)
+    type = Column(String)
 
     def __str__(self) -> str:
         return self.__tablename__
 
 
-class IdentityResourses(Base):
+class IdentityResourses(BaseModel):
     __tablename__ = "identity_resources"
 
     id = Column(Integer, primary_key=True)
-    description = Column(Text)
-    display_name = Column(String, nullable=False)
-    emphasize = Column(Boolean) # this record needs to be emphasized or not?
-    enabled = Column(Boolean, nullable=False)
-    name = Column(String)
-    required = Column(Boolean)
-    show_in_discovery_document = Column(Boolean)
+    description = Column(String(512), nullable=True)
+    display_name = Column(String(32), nullable=False)
+    emphasize = Column(Boolean, default=False, nullable=False)
+    enabled = Column(Boolean, default=False, nullable=False)
+    name = Column(String(32), nullable=False)
+    required = Column(Boolean, default=True, nullable=False)
+    show_in_discovery_document = Column(Boolean, default=False, nullable=False)
 
     def __str__(self) -> str:
         return self.__tablename__
--- src/data_access/postgresql/tables/persistent_grant_table.py
+++ src/data_access/postgresql/tables/persistent_grant_table.py
@@ -2,18 +2,30 @@ from datetime import datetime, timedelta
 
 from sqlalchemy import Column, Integer, DateTime, Text, PickleType, String
 from sqlalchemy.sql import func
+from sqlalchemy_utils import ChoiceType
 
-from .base import Base
+from .base import BaseModel
 
 
-class PersistentGrants(Base): 
+class PersistentGrants(BaseModel): 
     __tablename__ = "persistent_grants"
 
-    key = Column(String, unique=True, nullabe=False)
+    IDS_OF_SUBJECTS = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        #As many as needed
+    ]
+
+    TYPES_OF_GRANTS =[
+        ()
+    ]
+
+    key = Column(String(512), unique=True, nullabe=False)
     creation_time = Column(DateTime(timezone=True), server_default=func.now())
-    data = Column(PickleType)
-    subject_id = Column(Integer)
-    type = Column(Text)
+    data = Column(PickleType, nullable=False)
+    subject_id = Column(ChoiceType(IDS_OF_SUBJECTS), nullable=False)
+    type = Column(ChoiceType(TYPES_OF_GRANTS), nullable=False)
 
 
     @property
