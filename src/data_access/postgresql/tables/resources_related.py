c21b7a702b88008957bd8886428378a9efa30984
danya <d.krats@godeltech.com>
2022-11-24 16:45:31 +0100

    Changes according to review

--- src/data_access/postgresql/tables/resources_related.py
+++ src/data_access/postgresql/tables/resources_related.py
@@ -1,57 +1,87 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from sqlalchemy_utils import ChoiceType
 from .base import BaseModel
 
-class ApiResources(BaseModel):
+
+API_CLAIM_TYPE = []
+API_SCOPE_CLAIM_TYPE = [
+    ("name", "Name"),
+    ("family_name", "Family Name"),
+    ("middle_name", "Middle name"),
+    ("nickname", "Nickname"),
+    ("preferred_username", "Preferred username"),
+    ("profile_picture", "Profile picture"),
+    ("website", "Website"),
+    ("gender", "Gender"),
+    ("birthdate", "Birthdate"),
+    ("zone_info", "Zone info"),
+    ("locale", "Locale"),
+    ("updated_at", "Updated at")
+]
+API_SECRET_TYPE = [
+    ("sha256", "Sha256"),
+    ("sha512", "Sha512")
+]
+
+
+class ApiResource(BaseModel):
     __tablename__ = "api_resources"
 
     description = Column(String, nullable=True)
     display_name = Column(String, nullable=True)
     enabled = Column(Boolean, default=True, nullable=True)
     name = Column(String, nullable=False)
-    
+
     def __str__(self):
         return f"Model {self.__tablename__}: {self.id}"
 
-class ApiSecrets(BaseModel):
+
+class ApiSecret(BaseModel):
     __tablename__ = "api_secrets"
 
-    api_resources_id = Column("ApiResources", Integer, ForeignKey('ApiResources.id'))
+    api_resources_id = Column("ApiResource", Integer,
+                              ForeignKey('ApiResource.id'))
     description = Column(String, nullable=True)
     expiration = Column(DateTime, nullable=False)
-    type = Column(String, nullable=False)
+    type = Column(ChoiceType(API_SECRET_TYPE))
     value = Column(String, nullable=True)
 
     def __str__(self):
         return f"Model {self.__tablename__}: {self.id}"
 
-class ApiClaims(BaseModel):
+
+class ApiClaim(BaseModel):
     __tablename__ = "api_slaims"
-    
-    api_resources_id = Column("ApiResources", Integer, ForeignKey('ApiResources.id'))
-    type = Column(String, nullable=False)
-    
+
+    api_resources_id = Column("ApiResource", Integer,
+                              ForeignKey('ApiResource.id'))
+    type = Column(ChoiceType(API_CLAIM_TYPE))
+
     def __str__(self):
         return f"Model {self.__tablename__}: {self.id}"
 
-class ApiScopes(BaseModel):
+
+class ApiScope(BaseModel):
     __tablename__ = "api_scopes"
-    
-    api_resources_id = Column("ApiResources", Integer, ForeignKey('ApiResources.id'))
+
+    api_resources_id = Column("ApiResource", Integer,
+                              ForeignKey('ApiResource.id'))
     description = Column(String, nullable=True)
     name = Column(String, nullable=False)
     display_name = Column(String, nullable=True)
     emphasize = Column(Boolean, default=False, nullable=True)
     required = Column(Boolean, default=False, nullable=True)
-    show_in_discovery_document = Column(Boolean, default=False,  nullable=True)
+    show_in_discovery_document = Column(Boolean, default=False, nullable=True)
 
     def __str__(self):
         return f"Model {self.__tablename__}: {self.id}"
 
+
 class ApiScopeClaim(BaseModel):
     __tablename__ = "api_scope_claims"
-    
-    api_scopes_id = Column("ApiScopes", Integer, ForeignKey('ApiScopes.id'))
-    type = Column(String, nullable=True)
+
+    api_scopes_id = Column("ApiScope", Integer, ForeignKey('ApiScope.id'))
+    type = Column(ChoiceType(API_SCOPE_CLAIM_TYPE))
 
     def __str__(self):
         return f"Model {self.__tablename__}: {self.id}"
