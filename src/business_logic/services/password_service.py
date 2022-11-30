43eca1ba95d85001da08a11c9ed1f517561dad3b
Vasili Kulakou <v.kulakou@godeltech.com>
2022-11-30 15:54:41 +0100

    Create class for hash password

--- src/business_logic/services/password_service.py
+++ src/business_logic/services/password_service.py
@@ -4,6 +4,14 @@ import bcrypt
 class PasswordHash:
 
     @classmethod
-    def validate_password(cls, srt_password: str, hash_password: str):
-        pass
+    def hash_password(cls, password: str) -> bytes:
+        bts = password.encode('utf-8')
+        salt = bcrypt.gensalt()
 
+        return bcrypt.hashpw(bts, salt)
+
+    @classmethod
+    def validate_password(cls, str_password: str, hash_password: bytes) -> bool:
+        str_password_bytes = str_password.encode('utf-8')
+
+        return bcrypt.checkpw(str_password_bytes, hash_password)
