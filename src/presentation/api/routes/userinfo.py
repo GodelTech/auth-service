b7592a94a4c5946289b1c445ca8ddb13dea7850d
danya <d.krats@godeltech.com>
2022-12-09 11:34:29 +0100

    HTTPException Fix

--- src/presentation/api/routes/userinfo.py
+++ src/presentation/api/routes/userinfo.py
@@ -11,23 +11,23 @@ userinfo_router = APIRouter(
 
 @userinfo_router.get('/', response_model=ResponseUserInfoModel,)
 async def get_userinfo(request: RequestUserInfoModel = Depends(), userinfo_class: UserInfoServies = Depends()):
-    #try:
+    try:
         userinfo_class = userinfo_class
         userinfo_class.request = request
         return await userinfo_class.get_user_info()
-    #except:
-        raise HTTPException(status_code=500, detail="Incorrect Token")
+    except:
+        raise HTTPException(status_code=403, detail="Incorrect Token")
 
 
 @userinfo_router.get('/jwt', response_model=str,)
 async def get_userinfo_jwt(request: RequestUserInfoModel = Depends(), userinfo_class: UserInfoServies = Depends()):
-    #try:
+    try:
         userinfo_class = userinfo_class
         userinfo_class.request = request
         result = await userinfo_class.get_user_info_jwt()
         return result
-    #except:
-        raise HTTPException(status_code=500, detail="Incorrect Token")
+    except:
+        raise HTTPException(status_code=403, detail="Incorrect Token")
 
 @userinfo_router.get('/get_default_token', response_model=str,)
 async def get_default_token():
@@ -35,4 +35,4 @@ async def get_default_token():
         uis = UserInfoServies()
         return uis.jwt.encode_jwt()
     except:
-        raise HTTPException(status_code=500, detail="Incorrect Token")
+        raise HTTPException(status_code=500)
