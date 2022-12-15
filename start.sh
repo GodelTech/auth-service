52aa5445d29bac6112bf622a43a648a27af55ba6
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2022-12-15 12:05:45 +0300

    Modified databse url for container

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Environment
-export DATABASE_URL="postgresql+asyncpg://postgres:postgres@localhost/is_db"
+export DATABASE_URL="postgresql+asyncpg://postgres:postgres@postgres:5432/is_db"
 export SECRET_KEY="postgres"
 
 echo "Uvicorn running src.main:app --reload --host 0.0.0.0 --port 8000"
