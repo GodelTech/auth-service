3ccaa9ab031dc5dbb11b3a199bec8f9d72aad6a8
danya <d.krats@godeltech.com>
2022-12-23 14:34:43 +0100

    Revert "removed commens"
    This reverts commit eef26721f1232395ff3b06d71a211f105cf1f012.

--- src/business_logic/cache/key_builders.py
+++ src/business_logic/cache/key_builders.py
@@ -3,7 +3,10 @@ from fastapi_cache.backends.redis import RedisBackend
 from src.config.settings.cache_time import CacheTimeSettings
 from typing import Optional
 from fastapi import Request, Response
+# import logging
+# import redis
 
+# logger = logging.getLogger('is_app')
 
 def builder_with_parametr(
         func,
@@ -15,4 +18,10 @@ def builder_with_parametr(
 ):
     prefix = f'client-{request.client.host}'
     cache_key = f"{prefix}:{func.__module__}:{func.__name__}:{request.query_params}"
+    
+    # r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    # if r.get(cache_key):
+    #     logger.info(f'Used old Redis Key: "{cache_key}"')
+    # else:
+    #     logger.info(f'New Redis Key: "{cache_key}"  ')
     return cache_key
