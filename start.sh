56456f7c310652b39165852d1dc62f9861035d61
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-01-22 23:06:42 +0300

    Test

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Run migrations
-#echo "Migrations"
-#alembic upgrade heads
+echo "Migrations"
+alembic upgrade head
 
-echo "Uvicorn running src.main:app --reload --host 0.0.0.0 --port 8000"
-uvicorn src.main:app --reload --host 0.0.0.0 --port 8000
+echo "uvicorn src.main:app --host 0.0.0.0 --port 8000"
+uvicorn src.main:app --host 0.0.0.0 --port 8000
