905517a50303bfc9d751371fcda7cab5585439cf
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-01-23 19:28:18 +0300

    Init commit

---
+++ src/new_config.py
@@ -0,0 +1,24 @@
+import os
+
+from dynaconf import Dynaconf
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+settings = Dynaconf(
+    envvar_prefix="cinema",
+    preload=[os.path.join(APP_DIR, "default.toml")],
+    includes=["../configs/*.toml"],
+    environments=["local", "development", "docker"],
+    env_switcher="identity_server_poc_env",
+    load_dotenv=True,
+)
+
+
+DB_URI = settings.db.get("uri")
+
+
+REDIS_URL = settings.redis.get("host")
+REDIS_PORT = settings.redis.get("port")
+REDIS_CACHE_DB = settings.redis.get("cache_db")
+REDIS_USER = settings.redis.get("user")
+REDIS_PASSWORD = settings.redis.get("password")
