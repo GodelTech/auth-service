f5dda9b175ba71971d03f62f5643a0df7cd6c0e6
Karol Gajda <k.gajda@godeltech.com>
2023-02-06 09:53:14 +0100

    Add authentication to todos endpoint - test

--- client_example/main.py
+++ client_example/main.py
@@ -1,9 +1,9 @@
-from typing import Union
 from urllib.parse import urlencode
 
 import requests
-from fastapi import FastAPI, Header
+from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordBearer
 
 from .httpx_oauth.openid import OpenID
 
@@ -19,18 +19,48 @@ app = FastAPI()
 AUTH_URL = "http://localhost:8000/authorize/"
 TOKEN_URL = "http://localhost:8000/token/"
 USERINFO_URL = "http://localhost:8000/userinfo/"
+INTROSPECTION_URL = "http://localhost:8000/introspection/"
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='http://localhost:8000/token')
+
+
+def verify_access_token(token: str, credentials_exception):
+    headers = {
+        'authorization': f'Bearer {token}',
+    }
+    response = requests.get(USERINFO_URL, headers=headers)
+    if response.status_code == status.HTTP_401_UNAUTHORIZED:
+        raise credentials_exception
+    return response.json()
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return verify_access_token(token, credentials_exception)
+
+
+# ! Probably not implemented in /introspection
+async def is_token_valid(token: str) -> bool:
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    params = {"token": token, "token_type_hint": "refresh_token"}
+    response = requests.post(INTROSPECTION_URL, headers=headers, data=params)
+    return response.json()
 
 
 @app.get("/login")
-async def get_login_form(
-    authorization: Union[str, None] = Header(default=None)
-):
-    if authorization:
-        access_token = authorization.split(" ")[-1]
-        # verify token
-        # ...
-        # if valid:
-        return RedirectResponse("http://localhost:8001/")
+async def get_login_form(request: Request):
+    token = request.headers.get('Authorization')
+    if token and await is_token_valid(token.split(" ")[-1]):
+        return RedirectResponse("http://localhost:8001/elo")
+
     params = {
         "client_id": "test_client",
         "response_type": "code",
@@ -48,26 +78,28 @@ async def login_callback(code: str):
     token = await client.get_access_token(
         code=code, redirect_uri="http://localhost:8001/"
     )
+    # TODO store it on the client side
+    # 1. send token to frontend and store it
+    # Each time we receive token in a header we need to verify it
     access_token, refresh_token = token["access_token"], token["refresh_token"]
     # Use the access token to access the user info endpoint of the identity server
+    print(access_token)
+    print()
+    print(refresh_token)
     headers = {"Authorization": f"Bearer {access_token}"}
     userinfo_response = requests.get(USERINFO_URL, headers=headers)
     return {"message": "success", "profile": userinfo_response.json()}
 
 
 @app.get("/todos")
-async def get_todos(
-    authorization: Union[str, None] = Header(default=None),
-    auth_swagger: Union[str, None] = Header(
-        default=None, description="Authorization"
-    ),
-):
-    if authorization:
-        access_token = authorization.split(" ")[-1]
-    if auth_swagger is not None:
-        access_token = auth_swagger
-    # verify token
-    # ...
-    # if token is not valid or user don't have claims/permission raise an exception
-
+async def get_todos(user: dict = Depends(get_current_user)):
+    print(user.get('sub'))
     return {"todos": "blabla"}
+
+
+# ! to check if token is valid we can go to the /introspect endpoint
+
+
+# Check if token is valid
+# Try to refresh it?
+# pass again
