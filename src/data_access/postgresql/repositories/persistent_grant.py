c472d10c2ec72e6cdf7fe556b435f08cb41d7639
d.krats <d.krats@godeltech.com>
2023-02-13 19:06:06 +0100

    userinfo service

--- src/data_access/postgresql/repositories/persistent_grant.py
+++ src/data_access/postgresql/repositories/persistent_grant.py
@@ -10,7 +10,7 @@ from src.data_access.postgresql.errors.persistent_grant import (
     PersistentGrantNotFoundError,
 )
 from src.data_access.postgresql.repositories.base import BaseRepository
-from src.data_access.postgresql.tables.persistent_grant import PersistentGrant
+from src.data_access.postgresql.tables.persistent_grant import PersistentGrant, PersistentGrantType
 
 logger = logging.getLogger(__name__)
 
@@ -19,14 +19,16 @@ class PersistentGrantRepository(BaseRepository):
     async def create(
         self,
         client_id: str,
-        data: str,
+        grant_data: str,
         user_id: int,
-        grant_type: str = "code",
+        grant_type: int = 1,
         expiration_time: int = 600,
     ) -> None:
         session_factory = sessionmaker(
             self.engine, expire_on_commit=False, class_=AsyncSession
         )
+
+        # persistent_grant_id =  await self.get_type_id(grant_type)
         async with session_factory() as sess:
             session = sess
 
@@ -34,22 +36,36 @@ class PersistentGrantRepository(BaseRepository):
             persistent_grant = {
                 "key": unique_key,
                 "client_id": client_id,
-                "data": data,
+                "grant_data": grant_data,
                 "expiration": expiration_time,
                 "subject_id": user_id,
-                "type": grant_type,
+                "persistent_grant_type_id": grant_type,
             }
             # The sequence id number is out of sync and raises duplicate key error
             # We manually bring it back in sync
-            await session.execute(
-                text("SELECT setval(pg_get_serial_sequence('persistent_grants', 'id'), (SELECT MAX(id) FROM persistent_grants)+1);")
-            )
+            # await session.execute(
+            #     text("SELECT setval(pg_get_serial_sequence('persistent_grants', 'id'), (SELECT MAX(id) FROM persistent_grants)+1);")
+            # )
             await session.execute(
                 insert(PersistentGrant).values(**persistent_grant)
             )
             await session.commit()
+    
+    async def get_type_id(self, type_id: int) -> PersistentGrant:
+        session_factory = sessionmaker(
+            self.engine, expire_on_commit=False, class_=AsyncSession
+        )
+        async with session_factory() as sess:
+            session = sess
 
-    async def exists(self, grant_type: str, data: str) -> bool:
+            result = await session.execute(
+                select(PersistentGrantType).where(
+                    PersistentGrantType.id == type_id,
+                )
+            )
+            return result.first()[0].type_of_grant
+        
+    async def exists(self, grant_data: str) -> bool:
         session_factory = sessionmaker(
             self.engine, expire_on_commit=False, class_=AsyncSession
         )
@@ -59,8 +75,7 @@ class PersistentGrantRepository(BaseRepository):
             result = await session.execute(
                 select(
                     exists().where(
-                        PersistentGrant.type == grant_type,
-                        PersistentGrant.data == data,
+                        PersistentGrant.grant_data == grant_data,
                     )
                 )
             )
@@ -82,16 +97,14 @@ class PersistentGrantRepository(BaseRepository):
             )
             return result.first()[0]
 
-    async def delete(self, data: str, grant_type: str) -> int:
+    async def delete(self, grant_data: str) -> int:
         session_factory = sessionmaker(
             self.engine, expire_on_commit=False, class_=AsyncSession
         )
         async with session_factory() as sess:
             session = sess
-            if await self.exists(grant_type=grant_type, data=data):
-                grant_to_delete = await self.get(
-                    grant_type=grant_type, data=data
-                )
+            if await self.exists(grant_data=grant_data):
+                grant_to_delete = await self.get(grant_data=grant_data)
                 await session.delete(grant_to_delete)
                 await session.commit()
                 return status.HTTP_200_OK
