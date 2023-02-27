7bb474944bb4e5e67a2a3106a99f2648a9453d81
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-02-27 17:12:26 +0100

    Testing

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export ENV_FOR_DYNACONF="pipeline"
+export ENV_FOR_DYNACONF=$ENVIRONMENT_NAME
 echo "Environment: $ENV_FOR_DYNACONF"
 
 echo "uvicorn src.main:app --host 0.0.0.0 --port 8000"
