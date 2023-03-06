58f31c18fe4dccd88008fd9425dfc29bbfe6cb1a
Maksim Perkovskiy <m.perkovskiy@godeltech.com>
2023-03-06 17:17:11 +0000

    fix-pytest-and-remove-repeat-task

--- src/presentation/admin_api/models/group.py
+++ src/presentation/admin_api/models/group.py
@@ -4,17 +4,6 @@ from fastapi import Form
 from dataclasses import dataclass
 
 
-class RequestDefaultGroupModel(BaseModel):
-    group_id: int = Form(...)
-    class Config:
-        orm_mode = True
-
-@dataclass
-class RequestGroupModel:
-    group_id: int = Form(...)
-    class Config:
-        orm_mode = True
-
 
 @dataclass
 class RequestNewGroupModel:
@@ -25,7 +14,6 @@ class RequestNewGroupModel:
 
 @dataclass
 class RequestUpdateGroupModel:
-    group_id: int = Form(...)
     name: Union[None, str] = Form(None)
     parent_group: Union[None, int] = Form(None)
     class Config:
