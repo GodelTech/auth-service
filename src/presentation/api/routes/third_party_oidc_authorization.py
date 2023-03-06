ee26de608a3b79a983ef87b391afd6cf18ed7621
Vasili Kulakou <v.kulakou@godeltech.com>
2023-03-06 14:05:01 +0000

    27362-Add_3rd_party_Microsoft_provider

--- src/presentation/api/routes/third_party_oidc_authorization.py
+++ src/presentation/api/routes/third_party_oidc_authorization.py
@@ -11,6 +11,8 @@ from src.business_logic.services import (
     ThirdPartyGitLabService,
     ThirdPartyGoogleService,
     ThirdPartyLinkedinService,
+    ThirdPartyGitLabService,
+    ThirdPartyMicrosoftService,
 )
 from src.data_access.postgresql.errors import (
     ClientNotFoundError,
@@ -24,6 +26,7 @@ from src.di.providers import (
     provide_third_party_facebook_service_stub,
     provide_third_party_gitlab_service_stub,
     provide_third_party_google_service_stub,
+    provide_third_party_microsoft_service_stub,
 )
 from src.presentation.api.models import (
     StateRequestModel,
@@ -31,6 +34,7 @@ from src.presentation.api.models import (
     ThirdPartyGoogleRequestModel,
     ThirdPartyLinkedinRequestModel,
     ThirdPartyOIDCRequestModel,
+    ThirdPartyMicrosoftRequestModel,
 )
 
 logger = logging.getLogger(__name__)
@@ -80,7 +84,7 @@ async def get_github_authorize(
 
 
 @auth_oidc_router.get("/linkedin", status_code=status.HTTP_302_FOUND)
-async def get_linedin_authorize(
+async def get_linkedin_authorize(
     request_model: ThirdPartyLinkedinRequestModel = Depends(),
     auth_class: ThirdPartyLinkedinService = Depends(
         provide_auth_third_party_linkedin_service_stub
@@ -207,6 +211,43 @@ async def get_gitlab_authorize(
             raise WrongDataError
         response = RedirectResponse(
             github_redirect_uri, status_code=status.HTTP_302_FOUND
+        )
+        return response
+
+    except WrongDataError as exception:
+        logger.exception(exception)
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Wrong data has been passed"},
+        )
+    except IndexError as exception:
+        logger.exception(exception)
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": "Error in parsing"},
+        )
+
+
+@auth_oidc_router.get(
+    "/microsoft",
+    status_code=status.HTTP_302_FOUND,
+)
+async def get_microsoft_authorize(
+    request_model: ThirdPartyMicrosoftRequestModel = Depends(),
+    auth_class: ThirdPartyMicrosoftService = Depends(
+        provide_third_party_microsoft_service_stub
+    ),
+) -> Union[RedirectResponse, JSONResponse]:
+    try:
+        auth_class = auth_class
+        auth_class.request_model = request_model
+        redirect_uri = await auth_class.get_redirect_uri(
+            provider_name="microsoft"
+        )
+        if redirect_uri is None:
+            raise WrongDataError
+        response = RedirectResponse(
+            redirect_uri, status_code=status.HTTP_302_FOUND
         )
         return response
 
