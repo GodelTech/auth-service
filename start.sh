58e2c68a0b6ab7b109b08bd264cb959591b09e32
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-03-08 17:48:08 +0100

    Test

--- start.sh
+++ start.sh
@@ -7,18 +7,5 @@ export ENV_FOR_DYNACONF="pipeline"
 echo "Environment: $ENV_FOR_DYNACONF"
 
 
-if [ "$ENV_FOR_DYNACONF" = "test" ] || [ "$ENV_FOR_DYNACONF" = "pipeline" ]; then
-  echo "Test environment, no need for migrations"
-elif [ "$ENV_FOR_DYNACONF" = "local" ] || [ "$ENV_FOR_DYNACONF" = "development" ]; then
-  echo "Migrations"
-  alembic upgrade head
-  echo "Population of database"
-  python -m factories.commands
-else
-  echo "Migrations"
-  alembic upgrade head
-fi
-
-
 echo "uvicorn src.main:app --host 0.0.0.0 --port 8000"
 uvicorn src.main:app --host 0.0.0.0 --port 8000
