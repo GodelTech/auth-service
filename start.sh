da7b0c0804e8bc8cf71490aa12a16d517f8fe8b8
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-03-08 15:15:00 +0100

    Add db to kubernetes deploy

--- start.sh
+++ start.sh
@@ -3,8 +3,21 @@
 set -e
 
 # Environment
-export ENV_FOR_DYNACONF="pipeline"
+export ENV_FOR_DYNACONF=$ENVIRONMENT_NAME
 echo "Environment: $ENV_FOR_DYNACONF"
+
+
+if [ $ENV_FOR_DYNACONF = "test" ] || [ $ENV_FOR_DYNACONF = "pipeline" ]; then
+  echo "Test environment, no need for migrations"
+elif [ $ENV_FOR_DYNACONF = "local" ] || [ $ENV_FOR_DYNACONF = "development" ]; then
+  echo "Migrations"
+  alembic upgrade head
+  echo "Population of database"
+  python -m factories.commands
+else
+  echo "Migrations"
+  alembic upgrade head
+fi
 
 
 echo "uvicorn src.main:app --host 0.0.0.0 --port 8000"
