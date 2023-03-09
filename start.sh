d78ee2e3ed404759c2329de673c07089d8d48808
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-03-09 21:13:26 +0100

    Test

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ echo "Environment: $ENV_FOR_DYNACONF"
 if [ "$ENV_FOR_DYNACONF" = "test" ] || [ "$ENV_FOR_DYNACONF" = "pipeline" ]; then
   echo "Test environment, migration and population of database done inside test execution"
 elif [ "$ENV_FOR_DYNACONF" = "local" ] || [ "$ENV_FOR_DYNACONF" = "development" ]; then
-  echo "Migrations!"
+  echo "Migrations!!!"
   alembic upgrade head
   echo "Population of database"
   python -m factories.commands
