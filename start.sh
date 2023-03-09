dc81eec00b1232c96cb248584c2ca2864207d6c6
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-03-09 13:53:14 +0100

    Test

--- start.sh
+++ start.sh
@@ -3,25 +3,26 @@
 set -e
 
 # Environment
-if [ "$ENVIRONMENT_NAME" = "" ]; then
-  ENVIRONMENT_NAME="development"
-fi
-export ENV_FOR_DYNACONF=$ENVIRONMENT_NAME
-echo "Environment: $ENV_FOR_DYNACONF"
-
-
-if [ "$ENV_FOR_DYNACONF" = "test" ] || [ "$ENV_FOR_DYNACONF" = "pipeline" ]; then
-  echo "Test environment, migration and population of database done inside test execution"
-elif [ "$ENV_FOR_DYNACONF" = "local" ] || [ "$ENV_FOR_DYNACONF" = "development" ]; then
-  echo "Migrations !"
+#if [ "$ENVIRONMENT_NAME" = "" ]; then
+#  ENVIRONMENT_NAME="development"
+#fi
+#export ENV_FOR_DYNACONF=$ENVIRONMENT_NAME
+#echo "Environment: $ENV_FOR_DYNACONF"
+#
+#
+#if [ "$ENV_FOR_DYNACONF" = "test" ] || [ "$ENV_FOR_DYNACONF" = "pipeline" ]; then
+#  echo "Test environment, migration and population of database done inside test execution"
+#elif [ "$ENV_FOR_DYNACONF" = "local" ] || [ "$ENV_FOR_DYNACONF" = "development" ]; then
+#  echo "Migrations !"
+##  alembic upgrade head
+##  echo "Population of database"
+##  python -m factories.commands
+#else
+#  echo "Migrations"
 #  alembic upgrade head
-#  echo "Population of database"
-#  python -m factories.commands
-else
-  echo "Migrations"
-  alembic upgrade head
-fi
+#fi
 
+echo "Testing"
 
 echo "uvicorn src.main:app --host 0.0.0.0 --port 8000"
 uvicorn src.main:app --host 0.0.0.0 --port 8000
