b339832f19a7c6d45d11fba6257f72e952300778
Viktar Taustyka <vtaustyka@godeltech.com>
2023-03-27 09:43:40 +0200

    C1: several new tests in test_client_repository.py

--- tests/test_unit/test_repositories/test_client_repository.py
+++ tests/test_unit/test_repositories/test_client_repository.py
@@ -7,6 +7,12 @@ from src.data_access.postgresql.errors.client import (
 from src.data_access.postgresql.repositories.client import ClientRepository
 from sqlalchemy.ext.asyncio import AsyncEngine
 
+from src.data_access.postgresql.errors.client import (
+    ClientNotFoundError,
+    ClientPostLogoutRedirectUriError,
+    ClientRedirectUriError,
+)
+
 @pytest.mark.asyncio
 class TestClientRepository:
     async def test_get_client_by_client_id(self, engine: AsyncEngine) -> None:
@@ -14,13 +20,47 @@ class TestClientRepository:
         client = await client_repo.get_client_by_client_id(
             client_id="test_client"
         )
-        assert bool(client) is True
+        assert client.client_id == "test_client"
 
     async def test_get_client_by_client_id_not_exists(self, engine: AsyncEngine) -> None:
         client_repo_error = ClientRepository(engine)
         with pytest.raises(ClientNotFoundError):
             await client_repo_error.get_client_by_client_id(
                 client_id="test_client_not_exist"
+            )
+
+    @pytest.mark.parametrize("client_id, boolean",
+                             [("test_client", True),
+                              ("test_client_not_exist", False)])
+    async def test_validate_client_by_client_id(self, engine: AsyncEngine,
+                                                client_id,
+                                                boolean) -> None:
+        client_repo = ClientRepository(engine)
+        result = await client_repo.validate_client_by_client_id(
+            client_id=client_id
+        )
+        assert result == boolean
+
+    # validate_client_by_int_id differ for validate_client_by_client_id by
+    # containing ClientNotFoundError. Do we need one? Check in docs: 
+    # https://connect2id.com/products/server/docs/api/client-registration
+    @pytest.mark.parametrize("int_id, boolean",
+                             [(1, True),])
+                              # (999, False)])
+    async def test_validate_client_by_int_id(self, engine: AsyncEngine,
+                                                int_id,
+                                                boolean) -> None:
+        client_repo = ClientRepository(engine)
+        result = await client_repo.validate_client_by_int_id(
+            client_id=int_id
+        )
+        assert result == boolean
+
+    async def test_validate_client_by_int_id_not_exists(self, engine: AsyncEngine) -> None:
+        client_repo_error = ClientRepository(engine)
+        with pytest.raises(ClientNotFoundError):
+            await client_repo_error.validate_client_by_int_id(
+                client_id=999
             )
 
     async def test_get_client_secret_by_client_id(self, engine: AsyncEngine) -> None:
@@ -38,6 +78,23 @@ class TestClientRepository:
                 client_id="test_client_not_exist"
             )
 
+    async def test_validate_post_logout_redirect_uri(self, engine: AsyncEngine) -> None:
+        client_repo = ClientRepository(engine)
+        result = await client_repo.validate_post_logout_redirect_uri(
+            client_id="test_client",
+            logout_redirect_uri="http://thompson-chung.com/",
+        )
+        assert result
+
+    #### !!!!!!!!!!!!!mock ClientPostLogoutRedirectUriError ########################
+    async def test_validate_post_logout_redirect_uri_not_exists(self, engine: AsyncEngine) -> None:
+        client_repo_error = ClientRepository(engine)
+        with pytest.raises(ClientPostLogoutRedirectUriError):
+            await client_repo_error.validate_post_logout_redirect_uri(
+                client_id="test_client",
+                logout_redirect_uri="http://redirect-uri-not-exists.com/",
+            )
+
     async def test_validate_client_redirect_uri(self, engine: AsyncEngine) -> None:
         client_repo = ClientRepository(engine)
         uri = await client_repo.validate_client_redirect_uri(
@@ -51,3 +108,5 @@ class TestClientRepository:
             await client_repo.validate_client_redirect_uri(
                 client_id="test_client", redirect_uri="just_uri"
             )
+
+
