601029e7535c6d21d729ae28a112ea9f1e10f2f8
Aliaksandr Shyrokau <a.shyrokau@godeltech.com>
2023-05-07 16:35:44 +0200

    Add redirect to https

--- src/presentation/api/middleware/authorization_validation.py
+++ src/presentation/api/middleware/authorization_validation.py
@@ -5,9 +5,14 @@ from fastapi import status
 from fastapi.responses import JSONResponse
 from jwt.exceptions import PyJWTError
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import RedirectResponse
 from typing import Any, Callable
 from src.business_logic.services.jwt_token import JWTService
 from src.data_access.postgresql.repositories import BlacklistedTokenRepository
+from starlette.responses import RedirectResponse
+
+from src.dyna_config import IS_DEVELOPMENT
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,13 +26,19 @@ REQUESTS_WITH_AUTH = [
 
 
 class AuthorizationMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app: Any, blacklisted_repo:BlacklistedTokenRepository, jwt_service: JWTService = JWTService()) -> None:
+    def __init__(
+        self,
+        app: Any,
+        blacklisted_repo: BlacklistedTokenRepository,
+        jwt_service: JWTService = JWTService(),
+    ) -> None:
         self.app = app
         self.jwt_service = jwt_service
         self.blacklisted_repo = blacklisted_repo
 
-    async def dispatch_func(self, request: Any, call_next:Callable[..., Any]) -> Any:
-
+    async def dispatch_func(
+        self, request: Any, call_next: Callable[..., Any]
+    ) -> Any:
         for request_with_auth in REQUESTS_WITH_AUTH:
             if (
                 request_with_auth["path"] == request.url.path
@@ -44,15 +55,19 @@ class AuthorizationMiddleware(BaseHTTPMiddleware):
                         content="Incorrect Authorization Token",
                     )
                 if await self.blacklisted_repo.exists(
-                        token=token,
-                    ):
+                    token=token,
+                ):
                     return JSONResponse(
                         status_code=status.HTTP_401_UNAUTHORIZED,
-                        content="Token blacklisted"
+                        content="Token blacklisted",
                     )
                 try:
                     aud = request_with_auth["path"].split("/")[1]
-                    if not bool(await self.jwt_service.decode_token(token=token, audience=aud)):
+                    if not bool(
+                        await self.jwt_service.decode_token(
+                            token=token, audience=aud
+                        )
+                    ):
                         logger.exception("Authorization Failed")
                         return JSONResponse(
                             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -71,4 +86,6 @@ class AuthorizationMiddleware(BaseHTTPMiddleware):
         else:
             logger.info("No Authorization")
             response = await call_next(request)
+            if IS_DEVELOPMENT:
+                return RedirectResponse(request.url.replace(scheme="https"))
             return response
