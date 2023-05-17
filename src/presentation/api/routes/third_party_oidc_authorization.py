82abf297013d2e8b7ca6331d29db89bfa1606e5e
Karol Gajda <karol.gajda97@gmail.com>
2023-05-17 09:46:42 +0200

    Modify endpoints logic to pass the db session in a new way

--- src/presentation/api/routes/third_party_oidc_authorization.py
+++ src/presentation/api/routes/third_party_oidc_authorization.py
@@ -1,13 +1,20 @@
 import logging
 from typing import Union
 
-from fastapi import APIRouter, Depends, status
+from httpx import AsyncClient
+from fastapi import APIRouter, Depends, status, Request
 from fastapi.responses import JSONResponse, RedirectResponse
 
 from src.business_logic.services import ThirdPartyFacebookService
 from src.business_logic.third_party_auth import (
     ThirdPartyAuthServiceFactory,
     ThirdPartyAuthServiceProtocol,
+)
+from src.data_access.postgresql.repositories import (
+    ClientRepository,
+    ThirdPartyOIDCRepository,
+    UserRepository,
+    PersistentGrantRepository,
 )
 from src.business_logic.third_party_auth.dto import (
     StateRequestModel,
@@ -32,11 +39,18 @@ auth_oidc_router = APIRouter(
     status_code=status.HTTP_302_FOUND,
 )
 async def get_github_authorize(
+    request: Request,
     request_body: ThirdPartyAccessTokenRequestModel = Depends(),
-    auth_service_factory: ThirdPartyAuthServiceFactory = Depends(
-        provide_third_party_auth_service_factory_stub
-    ),
 ) -> Union[RedirectResponse, JSONResponse]:
+    session = request.state.session
+    auth_service_factory = ThirdPartyAuthServiceFactory(
+        session=session,
+        client_repo=ClientRepository(session),
+        user_repo=UserRepository(session),
+        persistent_grant_repo=PersistentGrantRepository(session),
+        oidc_repo=ThirdPartyOIDCRepository(session),
+        async_http_client=AsyncClient(),
+    )
     auth_service: ThirdPartyAuthServiceProtocol = (
         auth_service_factory.get_service_impl("github")
     )
@@ -46,11 +60,18 @@ async def get_github_authorize(
 
 @auth_oidc_router.get("/linkedin", status_code=status.HTTP_302_FOUND)
 async def get_linkedin_authorize(
+    request: Request,
     request_body: ThirdPartyAccessTokenRequestModel = Depends(),
-    auth_service_factory: ThirdPartyAuthServiceFactory = Depends(
-        provide_third_party_auth_service_factory_stub
-    ),
 ) -> Union[RedirectResponse, JSONResponse]:
+    session = request.state.session
+    auth_service_factory = ThirdPartyAuthServiceFactory(
+        session=session,
+        client_repo=ClientRepository(session),
+        user_repo=UserRepository(session),
+        persistent_grant_repo=PersistentGrantRepository(session),
+        oidc_repo=ThirdPartyOIDCRepository(session),
+        async_http_client=AsyncClient(),
+    )
     auth_service: ThirdPartyAuthServiceProtocol = (
         auth_service_factory.get_service_impl("linkedin")
     )
@@ -63,11 +84,18 @@ async def get_linkedin_authorize(
     status_code=status.HTTP_302_FOUND,
 )
 async def get_google_authorize(
+    request: Request,
     request_body: ThirdPartyAccessTokenRequestModel = Depends(),
-    auth_service_factory: ThirdPartyAuthServiceFactory = Depends(
-        provide_third_party_auth_service_factory_stub
-    ),
 ) -> Union[RedirectResponse, JSONResponse]:
+    session = request.state.session
+    auth_service_factory = ThirdPartyAuthServiceFactory(
+        session=session,
+        client_repo=ClientRepository(session),
+        user_repo=UserRepository(session),
+        persistent_grant_repo=PersistentGrantRepository(session),
+        oidc_repo=ThirdPartyOIDCRepository(session),
+        async_http_client=AsyncClient(),
+    )
     auth_service: ThirdPartyAuthServiceProtocol = (
         auth_service_factory.get_service_impl("google")
     )
@@ -80,11 +108,18 @@ async def get_google_authorize(
     status_code=status.HTTP_302_FOUND,
 )
 async def get_gitlab_authorize(
+    request: Request,
     request_body: ThirdPartyAccessTokenRequestModel = Depends(),
-    auth_service_factory: ThirdPartyAuthServiceFactory = Depends(
-        provide_third_party_auth_service_factory_stub
-    ),
 ) -> Union[RedirectResponse, JSONResponse]:
+    session = request.state.session
+    auth_service_factory = ThirdPartyAuthServiceFactory(
+        session=session,
+        client_repo=ClientRepository(session),
+        user_repo=UserRepository(session),
+        persistent_grant_repo=PersistentGrantRepository(session),
+        oidc_repo=ThirdPartyOIDCRepository(session),
+        async_http_client=AsyncClient(),
+    )
     auth_service: ThirdPartyAuthServiceProtocol = (
         auth_service_factory.get_service_impl("gitlab")
     )
@@ -97,11 +132,18 @@ async def get_gitlab_authorize(
     status_code=status.HTTP_302_FOUND,
 )
 async def get_microsoft_authorize(
+    request: Request,
     request_body: ThirdPartyAccessTokenRequestModel = Depends(),
-    auth_service_factory: ThirdPartyAuthServiceFactory = Depends(
-        provide_third_party_auth_service_factory_stub
-    ),
 ) -> Union[RedirectResponse, JSONResponse]:
+    session = request.state.session
+    auth_service_factory = ThirdPartyAuthServiceFactory(
+        session=session,
+        client_repo=ClientRepository(session),
+        user_repo=UserRepository(session),
+        persistent_grant_repo=PersistentGrantRepository(session),
+        oidc_repo=ThirdPartyOIDCRepository(session),
+        async_http_client=AsyncClient(),
+    )
     auth_service: ThirdPartyAuthServiceProtocol = (
         auth_service_factory.get_service_impl("microsoft")
     )
@@ -114,11 +156,18 @@ async def get_microsoft_authorize(
     status_code=status.HTTP_200_OK,
 )
 async def post_create_state(
+    request: Request,
     request_body: StateRequestModel = Depends(StateRequestModel.as_form),
-    auth_service_factory: ThirdPartyAuthServiceFactory = Depends(
-        provide_third_party_auth_service_factory_stub
-    ),
 ) -> JSONResponse:
+    session = request.state.session
+    auth_service_factory = ThirdPartyAuthServiceFactory(
+        session=session,
+        client_repo=ClientRepository(session),
+        user_repo=UserRepository(session),
+        persistent_grant_repo=PersistentGrantRepository(session),
+        oidc_repo=ThirdPartyOIDCRepository(session),
+        async_http_client=AsyncClient(),
+    )
     await auth_service_factory.create_provider_state(request_body.state)
     return JSONResponse(
         status_code=status.HTTP_200_OK,
@@ -126,36 +175,36 @@ async def post_create_state(
     )
 
 
-@auth_oidc_router.get(
-    "/facebook",
-    status_code=status.HTTP_302_FOUND,
-)
-async def get_facebook_authorize(
-    request_model: ThirdPartyFacebookRequestModel = Depends(),
-    auth_class: ThirdPartyFacebookService = Depends(
-        provide_third_party_facebook_service_stub
-    ),
-) -> Union[RedirectResponse, JSONResponse]:
-    try:
-        auth_class.request_model = request_model
-        github_redirect_uri = await auth_class.get_facebook_redirect_uri(
-            provider_name="facebook"
-        )
-        if github_redirect_uri is None:
-            raise WrongDataError
-        return RedirectResponse(
-            github_redirect_uri, status_code=status.HTTP_302_FOUND
-        )
+# @auth_oidc_router.get( # TODO not implemented because of https
+#     "/facebook",
+#     status_code=status.HTTP_302_FOUND,
+# )
+# async def get_facebook_authorize(
+#     request_model: ThirdPartyFacebookRequestModel = Depends(),
+#     auth_class: ThirdPartyFacebookService = Depends(
+#         provide_third_party_facebook_service_stub
+#     ),
+# ) -> Union[RedirectResponse, JSONResponse]:
+#     try:
+#         auth_class.request_model = request_model
+#         github_redirect_uri = await auth_class.get_facebook_redirect_uri(
+#             provider_name="facebook"
+#         )
+#         if github_redirect_uri is None:
+#             raise WrongDataError
+#         return RedirectResponse(
+#             github_redirect_uri, status_code=status.HTTP_302_FOUND
+#         )
 
-    except WrongDataError as exception:
-        logger.exception(exception)
-        return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content={"message": "Wrong data has been passed"},
-        )
-    except IndexError as exception:
-        logger.exception(exception)
-        return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content={"message": "Error in parsing"},
-        )
+#     except WrongDataError as exception:
+#         logger.exception(exception)
+#         return JSONResponse(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             content={"message": "Wrong data has been passed"},
+#         )
+#     except IndexError as exception:
+#         logger.exception(exception)
+#         return JSONResponse(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             content={"message": "Error in parsing"},
+#         )
