31ff95c662a4f7fbc2b097e4d2f6def7fe37ab02
Piotr Kuzma <piotr.adam.kuzma@gmail.com>
2023-06-06 12:31:09 +0200

    Add docstrings to WellKnownServices

--- src/business_logic/dto/open_id_config.py
+++ src/business_logic/dto/open_id_config.py
@@ -10,12 +10,10 @@ class OpenIdConfiguration(BaseModel):
     id_token_signing_alg_values_supported: list[str]
     subject_types_supported: list[str]
     response_types_supported: list[str]
-
     claims_supported: Optional[list[str]]
     scopes_supported: Optional[list[str]]
     registration_endpoint: Optional[str]
     userinfo_endpoint: Optional[str]
     end_session_endpoint: Optional[str]
     check_session_iframe: Optional[str]
-
     grant_types_supported: Optional[list[str]]
--- src/business_logic/services/well_known.py
+++ src/business_logic/services/well_known.py
@@ -15,9 +15,25 @@ logger = logging.getLogger(__name__)
 
 
 class WellKnownServices:
+    """A class that provides a metadata that an OAuth 2.0 client can use to
+    obtain the information needed to interact with an OAuth 2.0 authorization server
+    including its endpoint locations and authorization server capabilities.
+
+    More detalis about the functionality can be found in the
+    [official documentation](https://www.rfc-editor.org/rfc/rfc8414)
+    [openid connect discover 1.0](https://openid.net/specs/openid-connect-discovery-1_0.html)
+
+    """
+
     def __init__(
         self, session: AsyncSession, wlk_repo: WellKnownRepository
     ) -> None:
+        """Initialize the WellKnownServices.
+
+        Args:
+            session (AsyncSession): The session object.
+            wlk_repo (WellKnownRepository): The repository object.
+        """
         self.request: Union[Request, Any] = None
         self.wlk_repo = wlk_repo
         self.session = session
@@ -25,9 +41,26 @@ class WellKnownServices:
     def get_list_of_types(
         self, list_of_types: list[Any] = [("Not ready yet", "")]
     ) -> list[str]:
+        """Retrieves a list of types from a given list.
+
+        Args:
+            list_of_types (List[Any], optional): The list of types.
+            Defaults to [("Not ready yet", "")] as it is not implemented yet.
+
+        Returns:
+            List[str]: The list of types.
+        """
         return [claim[0] for claim in list_of_types]
 
     def get_all_urls(self, result: dict[str, Any]) -> dict[str, Any]:
+        """Retrieves all URLs from the request app routes.
+
+        Args:
+            result (Dict[str, Any]): The result dictionary.
+
+        Returns:
+            Dict[str, Any]: The dictionary of URLs containing all available endpoints.
+        """
         if self.request is None:
             raise ValueError
         return {
@@ -36,19 +69,62 @@ class WellKnownServices:
         } | {"false": "/ Not ready yet"}
 
     def get_algorithms(self) -> list[str]:
+        """Retrieves a list of algorithms from the JWTService.
+
+        Returns:
+            List[str]: The list of algorithms, currently contains only "RS256".
+        """
         return JWTService().algorithms
 
     async def get_claims(self) -> list[str]:
+        """Retrieves the user claim types from the repository.
+
+        Returns:
+            List[str]: The list of user claim types. Contains user`s parameters
+            like "name", "family_name", "gender", and the like.
+        """
         result = await self.wlk_repo.get_user_claim_types()
         return result
 
     async def get_grant_types(self) -> list[str]:
+        """Retrieves the grant types from the repository.
+
+        Returns:
+            List[str]: The list of grant types. Currently it contains "authorization_code",
+            "refresh_token", "client_credentials", "urn:ietf:params:oauth:grant-type:device_code".
+        """
         result = await self.wlk_repo.get_grant_types()
         return result
 
     async def get_openid_configuration(
         self,
     ) -> OpenIdConfiguration:
+        """
+        Retrieves the OpenID configuration class containing metadata decribing authorization servers
+        configuration.
+
+        Returns:
+            OpenIdConfiguration: The OpenID configuration in the form of pydantic model.
+            Required parameters:
+            - issuer - The authorization server's issuer identifier, which is a URL that uses the "https" scheme and has no query or fragment
+            components.  Authorization server metadata is published at a location that is ".well-known".
+            - authorization_endpoint - URL of the authorization server's authorization endpoint.
+            - token_endpoint -  URL of the authorization server's token endpoint.
+            - response_types_supported - JSON array containing a list of the OAuth 2.0
+            "response_type" values that this authorization server supports.
+            - id_token_signing_alg_values_supported - JSON array containing a list of
+            the JWS signing algorithms (alg values) supported by the OP for the ID Token to encode the Claims in a JWT.
+            - subject_types_supported - JSON array containing a list of the Subject Identifier types that this OP supports.
+            Not required parameters:
+            - claims_supported - JSON array containing a list of the Claim Names of the Claims that the OpenID Provider is able to supply values for.
+            - scopes_supported -  JSON array containing a list of the OAuth 2.0 scope values that this server supports.
+            - registration_endpoint - URL of the OP's Dynamic Client Registration Endpoint.
+            - userinfo_endpoint - URL of the OP's UserInfo Endpoint.
+            - end_session_endpoint - URL of the authorization server's endsession endpoint.
+            - check_session_iframe - parameter is used to enable the OpenID Connect session management feature.
+            - grant_types_supported - JSON array containing a list of the OAuth 2.0 Grant Type values that this OP supports.
+
+        """
         if self.request is None:
             raise ValueError
 
@@ -61,9 +137,7 @@ class WellKnownServices:
         result["jwks_uri"] = urls_dict["get_jwks"]
         result["authorization_endpoint"] = urls_dict["get_authorize"]
         result["id_token_signing_alg_values_supported"] = self.get_algorithms()
-        result["subject_types_supported"] = [
-            "public"
-        ]  # https://openid.net/specs/openid-connect-core-1_0.html#SubjectIDTypes
+        result["subject_types_supported"] = ["public"]
         result["response_types_supported"] = [
             "code",
             "token",
@@ -71,8 +145,6 @@ class WellKnownServices:
             "urn:ietf:params:oauth:grant-type:device_code",
         ]
         result["token_endpoint"] = urls_dict["get_tokens"]
-
-        # may be REQUIRED
         result["end_session_endpoint"] = urls_dict["end_session"]
         result["check_session_iframe"] = urls_dict["false"]
 
@@ -95,6 +167,17 @@ class WellKnownServices:
         return result_model
 
     async def get_jwks(self) -> dict[str, Any]:
+        """Retrieves the JWKS (JSON Web Key Set).
+
+        Returns:
+            Dict[str, Any]: The JWKS dictionary. Contains properties:
+            - kty - The family of cryptographic algorithms used with the key.
+            - alg - The specific cryptographic algorithm used with the key.
+            - use - How the key is meant to be used.
+            - n - The modulus for the RSA public key.
+            - e - The exponent for the RSA public key.
+
+        """
         jwt_service = JWTService()
         kty = ""
         if "RS" in jwt_service.algorithm:
