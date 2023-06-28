6c35aed976b63d251d6701bd84f93aa72c82d9f5
Piotr Kuźma <63551679+PureWater777@users.noreply.github.com>
2023-06-28 10:16:16 +0200

    Wellknown docstrings and fixes (#114)
    * Add initial dto for openidconfig and change business logic to acomodate class return type
    
    * Fix unit test for wellnkown, fix translation of pydantin model OpenIdConfiguration to dictionary
    
    * Add docstrings to WellKnownServices
    
    * Change name WellknownServices to WellknownService to adhere to convention
    
    * Possible pipeline fix
    
    * Typos before merge
    
    * Fix conftest import error

---
+++ src/business_logic/dto/open_id_config.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class OpenIdConfiguration(BaseModel):
+    issuer: str
+    jwks_uri: str
+    authorization_endpoint: str
+    token_endpoint: str
+    id_token_signing_alg_values_supported: list[str]
+    subject_types_supported: list[str]
+    response_types_supported: list[str]
+    claims_supported: Optional[list[str]]
+    scopes_supported: Optional[list[str]]
+    registration_endpoint: Optional[str]
+    userinfo_endpoint: Optional[str]
+    end_session_endpoint: Optional[str]
+    check_session_iframe: Optional[str]
+    grant_types_supported: Optional[list[str]]
